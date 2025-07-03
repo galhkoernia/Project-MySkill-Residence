e44c7860aa018636b78bed9e1d58665a21700928
Galuh Kurnia <galuhkoernia979@gmail.com>
2025-07-03 12:32:44 +0700

    update index, kontak, tentang kami, style css

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
-// Inisialisasi AOS (Animasi Scroll)
+
+// Inisialisasi Animation Click
 AOS.init();
 
-// Contoh animasi GSAP (opsional)
-gsap.from("h2", {
+// Efek animasi tambahan dengan GSAP (opsional)
+gsap.from("h2, h5", {
     duration: 1,
     y: 50,
     opacity: 0,
-    stagger: 0.3
+    stagger: 0.2
 });
+
+AOS.init();
+
+gsap.from("h4, h6", {
+    duration: 1,
+    y: 40,
+    opacity: 0,
+    stagger: 0.2
+});
+
+
